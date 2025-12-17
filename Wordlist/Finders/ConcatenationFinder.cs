9c56695d7aa665af1dadc33ecf14f94ad6a2b53d
David Gómez <david.gomez@external.toyota.es>
2025-12-17 22:14:54 +0100

    Working algorithm. Performance and edge cases to be reviewed

--- Wordlist/Finders/ConcatenationFinder.cs
+++ Wordlist/Finders/ConcatenationFinder.cs
@@ -1,12 +1,37 @@
-﻿using Wordlist.Models;
+﻿using Wordlist.Finders;
+using Wordlist.Models;
 
-namespace Wordlist.Finders
+namespace Wordlist
 {
     public sealed class WordConcatenationFinder : ICandidateWordsFinder
     {
         public IEnumerable<Concatenation> FindCandidates(ISet<string> words)
         {
-            throw new NotImplementedException();
+            ArgumentNullException.ThrowIfNull(words);
+
+            var set = new HashSet<string>(words);
+
+            var visited = new HashSet<string>();
+
+            foreach (var word in set)
+            {
+                if (word is null) continue;
+                if (word.Length != 6) continue;
+
+                for (int charCount = 1; charCount <= 5; charCount++)
+                {
+                    var left = word.Substring(0, charCount);
+                    var right = word.Substring(charCount);
+                    if (set.Contains(left) && set.Contains(right))
+                    {
+                        if (visited.Add(word))
+                        {
+                            yield return new Concatenation(left, right, word);
+                        }
+                        break;
+                    }
+                }
+            }
         }
     }
 }
